0216f40823a344026f357d4bb008113cf50e0aeb
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-12-11 17:23:42 -0500

    Initial

---
+++ HostPlugin.py
@@ -0,0 +1,40 @@
+import sys
+import PyPluMA
+
+# This script takes in the reads that are proposed by bmtagger to be human, and filteres them out of the original fastq file.
+
+# Note: Built from MetaWRAP (Uritskyi et al, 2018) "select_human_reads.py" script
+# Available at: https://github.com/bxlab/metaWRAP
+# Also under license from MIT, copy included
+
+class HostPlugin:
+    def input(self, filename):
+       self.parameters = dict()
+       for line in open(filename, 'r'):
+            line = line.strip()
+            contents = line.split('\t')
+            self.parameters[contents[0]] = contents[1]
+
+       # Load in the human reads:
+       self.human={}
+       for line in open(PyPluMA.prefix()+"/"+self.parameters["bmtaggerfile"]):
+          self.human[line.strip()]=None
+
+    def run(self):
+        pass
+
+    def output(self, filename):
+       # only print the human reads
+       skip=True
+       outfile = open(filename, 'w')
+       for i, line in enumerate(open(PyPluMA.prefix()+"/"+self.parameters["sequences"])):
+        if i%4==0: 
+         if line[1:].split("/")[0].split()[0] in self.human: skip=False
+         else: skip=True 
+
+        if skip==False: outfile.write(line.rstrip()+"\n")
+
+
+
+
+
